b2c6c24d7a0505864eb1433fdf6a79f6b08c0460
vscerlet <vscerlet_mail@tutamail.com>
2024-12-14 16:25:44 +0300

    Postponing a comment

--- pkg/ping/thread.go
+++ pkg/ping/thread.go
@@ -2,14 +2,13 @@ package ping
 
 import "sync"
 
-// PingingThread является пингующий горутиной, сначала сюда попадают адреса для проверки
-
 // Создаем WaitGroups для отслеживания завершения горутин
 var (
 	// PingWG - Группа горутин отвечающих за запуск/завершение пингующих потоков
 	PingWG sync.WaitGroup
 )
 
+// PingingThread является пингующий горутиной, сначала сюда попадают адреса для проверки
 func PingingThread(RawIPChannel chan string, IPChannel chan string) {
 	defer PingWG.Done()
 
