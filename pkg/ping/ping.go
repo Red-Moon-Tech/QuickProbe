fa8d9834d8ecf71952c31c748b93b4864a43bf57
vscerlet <vscerlet_mail@tutamail.com>
2024-12-15 21:50:14 +0300

    Small ping fix

--- pkg/ping/ping.go
+++ pkg/ping/ping.go
@@ -8,23 +8,34 @@ import (
 
 // Функция проверяет доступность хоста
 func pingTest(host string) bool {
+	// Инициализируем пингер
 	pinger, err := probing.NewPinger(host)
 	if err != nil {
 		panic(err)
 	}
 
-	pinger.Count = 1
+	// Устанавливаем тайм аут для пинга
 	pinger.Timeout = time.Millisecond * time.Duration(*argflags.Timeout)
+
+	// Устанавливаем количество попыток
+	pinger.Count = 1
+
+	// Устанавливаем непревилигированный доступ
+	pinger.SetPrivileged(false)
 
 	// Отключение отладочной информации
 	pinger.Debug = false
 
+	// Запускаем пингование
 	err = pinger.Run()
 	if err != nil {
 		panic(err)
 	}
 
+	// Получаем статистику
 	stats := pinger.Statistics()
+
+	// Возвращаем результат
 	if stats.PacketsRecv != 0 {
 		return true
 	} else {
--- pkg/statistic/ping.go
+++ pkg/statistic/ping.go
@@ -12,20 +12,31 @@ func pingThread(ctx context.Context) {
 		case <-ctx.Done():
 			return
 		default:
+			// Инициализируем пингер
 			pinger, err := probing.NewPinger("8.8.8.8")
 			if err != nil {
 				panic(err)
 			}
 
-			pinger.Count = 1
+			// Устанавливаем количество попыток
+			pinger.Count = 4
 
+			// Устанавливаем непревилигированный доступ
+			pinger.SetPrivileged(false)
+
+			// Отключение отладочной информации
+			pinger.Debug = false
+
+			// Запускаем пингование
 			err = pinger.Run()
 			if err != nil {
 				panic(err)
 			}
 
+			// Получаем статистику
 			stats := pinger.Statistics()
 
+			// Сохраняем задержку в переменную
 			StatisticMutex.Lock()
 			if stats.PacketsRecv != 0 {
 				PingStatus = uint64(stats.MaxRtt.Milliseconds())
@@ -33,6 +44,8 @@ func pingThread(ctx context.Context) {
 				PingStatus = 0
 			}
 			StatisticMutex.Unlock()
+
+			// Ждём до следующей итерации
 			time.Sleep(time.Millisecond * 100)
 		}
 	}
